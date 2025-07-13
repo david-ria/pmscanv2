33fc10b74f3175a42dc89e95f7409faa6473e477
david-ria <david@riallant.com>
2025-07-13 19:18:38 +0200

    Add discard mission toast i18n

--- src/components/FloatingRecordButton.tsx
+++ src/components/FloatingRecordButton.tsx
@@ -93,7 +93,7 @@ export function FloatingRecordButton({ device, className }: FloatingRecordButton
     
     toast({
       title: t('modals.missionDetails.delete'),
-      description: t('analysis.error'),
+      description: t('modals.missionDetails.discarded'),
     });
   };
 
