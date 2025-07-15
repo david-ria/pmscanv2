6ce54ba00727e750cdaa48c80274367849c3a055
david-ria <david@riallant.com>
2025-07-15 14:37:07 +0200

    Remove React StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -15,20 +14,18 @@ import './i18n/config';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AuthProvider>
-          <ThresholdProvider>
-            <AlertProvider>
-              <ThemeProvider>
-                <App />
-                <Toaster />
-              </ThemeProvider>
-            </AlertProvider>
-          </ThresholdProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <AuthProvider>
+        <ThresholdProvider>
+          <AlertProvider>
+            <ThemeProvider>
+              <App />
+              <Toaster />
+            </ThemeProvider>
+          </AlertProvider>
+        </ThresholdProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
